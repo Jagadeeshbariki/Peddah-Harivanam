11e4b9df392196f2428eaec8249647877cd8fdf5
Jagadeesh <jagadeesh@kali.Jagadeesh>
2023-11-16 17:43:53 +0530

    my first commit

---
+++ pedda.js
@@ -0,0 +1,15 @@
+function displayImg(imgPath) {
+    var modal = document.getElementById('myModal');
+    var displayedImage = document.getElementById('displayedImage');
+
+    displayedImage.src = imgPath;
+    modal.style.display = 'block';
+}
+
+function hideModal() {
+    document.getElementById('myModal').style.display = 'none';
+}
+
+function jagadeesh(){
+    alert("working fine");
+}
